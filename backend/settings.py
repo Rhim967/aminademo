49eb3bfe02f4ca22feea0392f2b8b0ff17f81a39
Rahim <rakhim.199206@gmail.com>
2021-11-23 22:16:00 +0300

    added whitenoise in middlewere

--- backend/settings.py
+++ backend/settings.py
@@ -90,7 +90,7 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
