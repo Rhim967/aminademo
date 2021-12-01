e8b39871bfb2c01cc3cd046bddb9ea3cc9b9dba2
Rahim <rakhim.199206@gmail.com>
2021-12-01 08:18:41 +0300

    added host

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY') #'django-insecure-177%tyn36h*b^h-tfjh3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'demombb.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'demombb.herokuapp.com', 'aminademo.herokuapp.com']
 
 
 # Application definition
